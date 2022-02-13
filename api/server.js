d78431e46ebc4805d735920a8d28c59ccc95a246
psideris <psideris1989@gmail.com>
2022-02-13 18:34:06 +0000

    Create react-app and express server

---
+++ api/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const port = '8080';
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
